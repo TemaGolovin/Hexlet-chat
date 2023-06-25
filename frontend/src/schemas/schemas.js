37020e8b70055d90fff74cd1dd37e0329f4e329f
TemaGolovin <golart1996@mail.ru>
2023-06-25 12:16:39 +0700

    add login schema

---
+++ frontend/src/schemas/schemas.js
@@ -0,0 +1,23 @@
+import * as yup from "yup";
+
+export const SignupSchema = (nameMsg, passwordMsg, equalMsg, requiredMsg) =>
+  yup.object().shape({
+    username: yup
+      .string()
+      .trim()
+      .min(3, nameMsg)
+      .max(20, nameMsg)
+      .required(requiredMsg),
+    password: yup.string().trim().min(5, passwordMsg).required(requiredMsg),
+    confirmPassword: yup
+      .string()
+      .trim()
+      .oneOf([yup.ref("password")], equalMsg)
+      .required(requiredMsg),
+  });
+
+export const SigninSchema = (message) =>
+  yup.object().shape({
+    username: yup.string().trim().required(message),
+    password: yup.string().trim().required(message),
+  });
