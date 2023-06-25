7cdcbdfcabf3a480ef50f9f62a0e7867453d3e2a
TemaGolovin <golart1996@mail.ru>
2023-06-25 12:21:19 +0700

    add locales

---
+++ frontend/src/locales/ru.js
@@ -0,0 +1,17 @@
+export default {
+  translation: {
+    entry: "Войти",
+    makeRegistration: "Регистрация",
+    noAccount: "Нет аккаунта?",
+    invalidFeedback: "Неверные имя пользователя или пароль",
+    pageNotFound: "Страница не найдена",
+    placeholders: {
+      yourNickname: "Ваш ник",
+      password: "Пароль",
+    },
+    errors: {
+      required: "Обязательное поле",
+      invalidFeedback: "Неверные имя пользователя или пароль",
+    },
+  },
+};
