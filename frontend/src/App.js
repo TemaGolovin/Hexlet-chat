e9a71a78f47bfb3458bb556abe2df3a6114aa9a2
TemaGolovin <golart1996@mail.ru>
2023-06-25 12:02:06 +0700

    add defaultRoute

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import NotFound from "./components/NotFound.jsx";
 import Login from "./components/Login/Login.jsx";
+import ChatPage from "./components/Chat/ChatPage.jsx";
 import { appPaths } from "./routes.js";
+import { useAuth } from "./hooks";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+const DefaultRoute = ({ children }) => {
+  const auth = useAuth();
+  return auth.user ? children : <Navigate to={appPaths.login} />;
+};
 
 function App() {
   return (
@@ -11,6 +19,14 @@ function App() {
       <Routes>
         <Route path={appPaths.notFound} element={<NotFound />} />
         <Route path={appPaths.login} element={<Login />} />
+        <Route
+          path={appPaths.chat}
+          element={
+            <DefaultRoute>
+              <ChatPage />
+            </DefaultRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
