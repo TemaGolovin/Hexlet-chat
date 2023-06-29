72259b050e9e3905e9f36359fffa73df7bc66a1b
TemaGolovin <golart1996@mail.ru>
2023-06-29 11:13:39 +0700

    add store

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -4,17 +4,21 @@ import { I18nextProvider, initReactI18next } from "react-i18next";
 import App from "./App.js";
 import resources from "./locales";
 import AuthProvider from "./context/AuthProvider.jsx";
+import { Provider as StoreProvider } from "react-redux";
+import store from "./store";
 
 const init = async () => {
   const i18n = i18next.createInstance();
 
   await i18n.use(initReactI18next).init({ resources, fallbackLng: "ru" });
   return (
-    <AuthProvider>
-      <I18nextProvider i18n={i18n}>
-        <App />
-      </I18nextProvider>
-    </AuthProvider>
+    <StoreProvider store={store}>
+      <AuthProvider>
+        <I18nextProvider i18n={i18n}>
+          <App />
+        </I18nextProvider>
+      </AuthProvider>
+    </StoreProvider>
   );
 };
 
