fed92c866466808d2348a4f307c5e3520fc03613
TemaGolovin <golart1996@mail.ru>
2023-06-29 11:11:19 +0700

    add chatPage

--- frontend/src/components/Chat/ChatPage.jsx
+++ frontend/src/components/Chat/ChatPage.jsx
@@ -1,7 +1,31 @@
-import React /*useEffect, useRef,*/ from "react";
+import React /*useEffect useRef,*/ from "react";
+import { Container, Row } from "react-bootstrap";
+import ChatBox from "./ChatBox.jsx";
+
+import { useDispatch } from "react-redux";
+import { useEffect, useMemo } from "react";
+import fetchDataThunk from "../../store/slices/thunk";
+import { useAuth } from "../../hooks";
 
 const ChatPage = () => {
-  return <p>Здесь будет чат</p>;
+  const dispatch = useDispatch();
+  const { getAuthHeader } = useAuth();
+  const authHeaders = useMemo(
+    () => ({ headers: getAuthHeader() }),
+    [getAuthHeader]
+  );
+
+  useEffect(() => {
+    dispatch(fetchDataThunk(authHeaders));
+  }, [dispatch]);
+
+  return (
+    <Container className="h-100 my-4 overflow-hidden rounded shadow">
+      <Row className="h-100 bg-white flex-md-row">
+        <ChatBox />
+      </Row>
+    </Container>
+  );
 };
 
 export default ChatPage;
