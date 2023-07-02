6c65d0669373a0d95954fb61541ce61569db4e47
TemaGolovin <golart1996@mail.ru>
2023-07-02 10:33:13 +0700

    add websoket

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 
-import { AuthContext } from "../context/index.js";
+import { AuthContext, SocketContext } from "../context/index.js";
 
 export const useAuth = () => useContext(AuthContext);
+export const useSocket = () => useContext(SocketContext);
