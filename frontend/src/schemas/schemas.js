10f56d1f980ebe5eb84c8dbd64ef4056dc830102
TemaGolovin <golart1996@mail.ru>
2023-07-02 10:43:30 +0700

    add addChannelSchema

--- frontend/src/schemas/schemas.js
+++ frontend/src/schemas/schemas.js
@@ -8,7 +8,7 @@ export const SignupSchema = (nameMsg, passwordMsg, equalMsg, requiredMsg) =>
       .min(3, nameMsg)
       .max(20, nameMsg)
       .required(requiredMsg),
-    password: yup.string().trim().min(5, passwordMsg).required(requiredMsg),
+    password: yup.string().trim().min(6, passwordMsg).required(requiredMsg),
     confirmPassword: yup
       .string()
       .trim()
@@ -25,4 +25,15 @@ export const SigninSchema = (message) =>
 export const messageShema = (message) =>
   yup.object().shape({
     messageText: yup.string().trim().required(message),
+  });
+
+export const addChannelSchema = (channels, doubleMsg, lengthMsg) =>
+  yup.object().shape({
+    body: yup
+      .string()
+      .trim()
+      .required(lengthMsg)
+      .min(3, lengthMsg)
+      .max(20, lengthMsg)
+      .notOneOf(channels, doubleMsg),
   });
