f9d34e51ee5d33af3e43de86df33eaee31237aff
TemaGolovin <golart1996@mail.ru>
2023-07-04 12:51:51 +0700

    add censoring

--- frontend/src/components/Chat/Messages/Messages.jsx
+++ frontend/src/components/Chat/Messages/Messages.jsx
@@ -1,9 +1,13 @@
+import { useWordFilter } from "../../../hooks";
+
 const Messages = ({ message }) => {
+  const filterProfanity = useWordFilter();
+
   return (
     <div className="text-break mb-2">
       <b>{message.user}</b>
       {": "}
-      {message.body}
+      {filterProfanity(message.body)}
     </div>
   );
 };
