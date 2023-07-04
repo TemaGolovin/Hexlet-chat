1b53826d1ada05794443288bb1621dae3bbfe1c6
TemaGolovin <golart1996@mail.ru>
2023-07-04 12:57:02 +0700

    add WordFilterProvider

---
+++ frontend/src/context/WordFilterProvider.jsx
@@ -0,0 +1,18 @@
+import filter from "leo-profanity";
+import { useCallback } from "react";
+
+import { WordFilterContext } from "./index";
+
+const WordFilterProvider = ({ children }) => {
+  filter.add(filter.getDictionary("ru"));
+
+  const filterProfanity = useCallback((word) => filter.clean(word), []);
+
+  return (
+    <WordFilterContext.Provider value={filterProfanity}>
+      {children}
+    </WordFilterContext.Provider>
+  );
+};
+
+export default WordFilterProvider;
