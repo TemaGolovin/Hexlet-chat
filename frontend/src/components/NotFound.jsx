c55ca2f616e433607c2210acd416b26ed4b67b63
TemaGolovin <golart1996@mail.ru>
2023-07-05 17:54:15 +0700

    update notfound page

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,5 +1,28 @@
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { appPaths } from "../routes";
+import { useTranslation } from "react-i18next";
+
 const NotFound = () => {
-  return <p>Страница не найдена, error 404</p>;
+  const navigate = useNavigate();
+  const { t } = useTranslation();
+
+  return (
+    <Container fluid className="h-100">
+      <Row className="justify-content-center align-content-center h-100">
+        <Col className="text-center">
+          <p className="display-1">{t("codeNotFound")}</p>
+          <p className="display-5 mb-4">{t("pageNotFound")}</p>
+          <Button
+            variant="outline-primary "
+            onClick={() => navigate(appPaths.chat)}
+          >
+            {t("goMainPage")}
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default NotFound;
