bfc27c27cf59aeff459521e97f44c99182e771ba
TemaGolovin <golart1996@mail.ru>
2023-07-05 17:54:49 +0700

    add error page

---
+++ frontend/src/components/Chat/Error.jsx
@@ -0,0 +1,37 @@
+import { useSelector } from "react-redux";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+
+import { Button } from "react-bootstrap";
+
+import { apiRoutes } from "../../routes";
+import { useAuth } from "../../hooks";
+
+const Error = () => {
+  const navigate = useNavigate();
+  const { t } = useTranslation();
+  const { logOut } = useAuth();
+  const { errorCode, errorMessage } = useSelector(
+    (state) => state.channels.error
+  );
+
+  const handleAuthError = () => {
+    navigate(apiRoutes.login);
+    logOut();
+  };
+
+  const handleOtherError = () => {
+    navigate(0);
+  };
+
+  return (
+    <div className="m-auto w-auto text-center">
+      <h3>{t("error")}</h3> <p>{errorMessage}</p>
+      <Button onClick={errorCode === 401 ? handleAuthError : handleOtherError}>
+        {errorCode === 401 ? t("reauthorization") : t("update")}
+      </Button>
+    </div>
+  );
+};
+
+export default Error;
